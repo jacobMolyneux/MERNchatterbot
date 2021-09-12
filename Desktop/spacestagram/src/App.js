b2886d37c3bd7ea1d7a93b56403d24df8b939b6e
Jacob Molyneux <79246480+jacobMolyneux@users.noreply.github.com>
2021-09-12 16:32:45 -0400

    first commit

--- Desktop/spacestagram/src/App.js
+++ Desktop/spacestagram/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Navbar } from "./components/navbar";
+import { ImageCard } from "./components/card.js";
+
+const apiKey =
+  "https://api.nasa.gov/planetary/apod?api_key=GPUGbLvmJxTiOY0ftc1SVstnldjZmkaQoc0IWtBV&count=10";
 
 function App() {
+  let [Images, setImages] = useState([]);
+  let [spaceState, setSpaceState] = useState(true);
+  let [marsState, setMarsState] = useState(false);
+  useEffect(() => {
+    fetch(apiKey)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setImages(Images.concat(data));
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Navbar />
+      <div>
+        <button
+          className="SearchButton"
+          id="spaceSearchBtn"
+          onClick={() => setSpaceState((spaceState = true))}
         >
-          Learn React
-        </a>
-      </header>
+          Space Images
+        </button>
+        <button
+          className="searchButton"
+          id="MarsSearchBtn"
+          onClick={() => setMarsState((marsState = true))}
+        >
+          Mars Images
+        </button>
+      </div>
+      <div id="container">
+        {Images.map((Image) => (
+          <ImageCard
+            title={Image.title}
+            image={Image.hdurl}
+            date={Image.date}
+            description={Image.explanation}
+          />
+        ))}
+      </div>
     </div>
   );
 }
