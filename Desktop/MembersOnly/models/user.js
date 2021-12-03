1686d27d7aa106beed954118625f09c1c8e9e5c3
Jacob Molyneux <79246480+jacobMolyneux@users.noreply.github.com>
2021-12-02 23:31:53 -0500

    Delete Desktop/MembersOnly directory

--- Desktop/MembersOnly/models/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-
-var UserSchema = new Schema({
-  password: { type: String, required: true, minLength: 5, maxLength: 32 },
-  username: { type: String, required: true },
-});
-
-module.exports = mongoose.model("User", UserSchema);
